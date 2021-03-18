2e4416de385429b75026dbc201fd9246c72d7309
hg48882 <73612075+hg48882@users.noreply.github.com>
2021-03-18 02:27:04 +0100

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<nav id="nav">
+	<ul>
+		<li class="special">
+			<a href="#menu" class="menuToggle"><span>Menu</span></a>
+			<div id="menu">
+				<ul>
+					<li><a href="index.php" <?php echo ($_SERVER['PHP_SELF'] == '/index.php' ? 'active' : '');?>>Home</a></li>
+					<li><a href="meditation.php">Meditation 101</a></li>
+					<li><a href="scientific.php">Scientific Research</a></li>
+					<!-- <li><a href="retreats.php">Retreats</a></li> -->
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="loginregister.php">Log In / Register</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+				</ul>
+			</div>
+		</li>
+	</ul>
+</nav>
