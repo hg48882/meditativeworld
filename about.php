3d412087bd316597c981a9148b027bc614fffff2
hg48882 <73612075+hg48882@users.noreply.github.com>
2021-03-18 02:25:44 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>About Us</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		
+			<div id="page-wrapper">
+
+					<header id="header">
+						<h1><a href="index.html">Meditative World</a></h1>
+						
+                    <?php include 'includes/navbar.php' ?>
+                    
+					</header>
+
+				
+					<article id="main">
+						<header>
+							<h2>About Meditative World &copy;</h2>
+							<p>...</p>
+						</header>
+						<section class="wrapper style5">
+							<div class="inner">
+
+                                <h3>Who we are?</h3>
+                                <p>Meditative World was officially launched in 2020 in Prishtina, Kosovo. A small team of people coming from
+                                    the Western Balkans who have been practicing meditation for years, decided to share their techniques and 
+                                    experience of the benefits of meditation with people around the globle, especially targeting the busy western lifestyle.
+                                    Meditative World aims at helping people improve their mental health and wellbeing.</p>
+                
+								<hr />
+
+							</div>
+						</section>
+					</article>
+
+				
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; 2021 Meditative World</li></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
